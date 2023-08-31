c3cfe30e0b1ec56ec062bc832b5339a777b1c0c8
Swapnil Jadhav <swapnil.jadhav@avisys.in>
2023-08-31 14:18:13 +0530

    validations added in company details and educational qualifications

--- src/app/modules/main/components/forms/educational-qualification-form/educational-qualification-form.component.ts
+++ src/app/modules/main/components/forms/educational-qualification-form/educational-qualification-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { EducationalQualificationService } from '../../../services/educational-qualification.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { EducationalQualification } from '../../../models/educational-qualification.model';
 import { CommonMaster } from '../../../models/common-master.model';
+import { CustomValidators } from '../../../services/custom-validators.service';
 
 @Component({
   selector: 'app-educational-qualification-form',
@@ -52,10 +53,29 @@ export class EducationalQualificationFormComponent implements OnInit {
   initForm() {
     this.educationalDetailsForm = this.formBuilder.group({
       id: [''],
-      educationalQualification: [''],
-      qualificationLevel: [''],
-      instituteName: [''],
-      passingYear: [''],
+      educationalQualification: [
+        '',
+        [
+          Validators.required,
+          CustomValidators.noLeadingSpace(),
+          CustomValidators.whitespaceValidator(),
+          CustomValidators.noTrailingSpace(),
+          CustomValidators.maxLength(50),
+          Validators.pattern('^[a-zA-Z.-]{1,50}$'),
+        ],
+      ],
+      qualificationLevel: ['', Validators.required],
+      instituteName: [
+        '',
+        [
+          Validators.required,
+          CustomValidators.noLeadingSpace(),
+          CustomValidators.noTrailingSpace(),
+          CustomValidators.maxLength(100),
+          Validators.pattern('^[a-zA-Z.-]{1,100}$'),
+        ],
+      ],
+      passingYear: ['', Validators.required],
       orgCode: ['AVI-01'],
       createdBy: ['Admin'],
       updatedBy: ['Admin'],
@@ -140,5 +160,19 @@ export class EducationalQualificationFormComponent implements OnInit {
   resetForm() {
     this.collectQueryParams();
     this.initForm();
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.educationalDetailsForm.get(controlName);
+    return !!control && control.invalid && control.touched;
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.educationalDetailsForm.get(controlName);
+    if (control && control.errors) {
+      const errorKey = Object.keys(control.errors)[0];
+      return CustomValidators.getErrorMessage(errorKey, controlName);
+    }
+    return '';
   }
 }
