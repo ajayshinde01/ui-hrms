b5b04a05f63d82931130af1080d1aad6b4db247b
AniVaishampayan <aniket.vaishampayan@avisys.in>
2023-09-05 18:47:57 +0530

    Email Changes

--- src/app/modules/master/components/forms/Validations/blankData.validator.ts
+++ src/app/modules/master/components/forms/Validations/blankData.validator.ts
@@ -1,18 +1,4 @@
-// import { FormControl, ValidationErrors } from '@angular/forms';
 
-// export function blankValidator(control: FormControl): ValidationErrors | null {
-//   const value = control.value as string;
-//   console.log(value);
-//   if (value && value.trim() === '') {
-//     // Return a validation error object
-//     return { blank: true };
-//   }
-
-//   // If the value is not blank, return null to indicate no validation error
-//   return null;
-
-//   //return value.includes(' ') ? { blankData: true } : null;
-// }
 
 
 
